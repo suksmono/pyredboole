a5277bf98d7e685a85f92799f635a9cc6f202ce7
suksmono <38452529+suksmono@users.noreply.github.com>
2018-06-26 14:05:06 +0700

    Add files via upload

--- learn_perdomoS12.py
+++ learn_perdomoS12.py
@@ -16,12 +16,8 @@ THE PROBLEM
 '''
 S12= 4 -3*q1 + 4*q2 -4*q1*q2 -q3 + q1*q3 -2*q2*q3 +4*q4 -2*q1*q4 - 8*q2*q4 + 5*q1*q2*q4 -2*q3*q4 + 5*q2*q3*q4 - q1*q2*q3*q4
 
-
 ''' 
 k-body -> 2-body
-function H2sub= H2body(x1,x2,y,d12)
-% H_5(q1,q2,Q5;d5)=d5*(3*Q5+q1*q2-2*q1*Q5-2*q2*q5)
-H2sub=d12*(3*y+x1*x2-2*x1*y-2*x2*y);
 '''
 def H2sub(x1,x2,y,d12):
     return(d12*(3*y+x1*x2-2*x1*y-2*x2*y))
@@ -44,10 +40,9 @@ q-domain, 2-body interaction:
 ''' 
 print('\n q-domain, 2-body interaction:\n',S12_2bdy)    
 
-# define functions: q2s, s2q
+# define functions: q2s
 
 def q2s(x):
-#    return((1/2)*(1-x))
     return(1/2 -x/2)
     
 S12s=simplify( S12_2bdy.subs(
